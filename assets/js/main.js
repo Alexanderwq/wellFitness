06d3ff47eb5a8b509d00f7aa6d824a44b725e347
Alexanderwq <lokistapway@yandex.ru>
2021-10-20 23:17:51 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// product slider
+
+let productSlider = new Swiper(".products-content", {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    
+})
+
+// NEWS slider
+
+let newsSlider = new Swiper(".news__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+})
+
+// brands slider
+
+let brandsNavSlider = new Swiper(".brands-categories", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+})
+
+// IDEA slider
+
+let ideaSlider = new Swiper(".idea-content", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+})
+
+
